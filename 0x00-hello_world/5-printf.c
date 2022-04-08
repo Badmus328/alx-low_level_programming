8a0bc893912ecfaf4693d71d52c8871ce7a8baa7
Badmus328 <badmusridwa965>
2022-04-08 07:25:58 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,26 +1,13 @@
 #include <stdio.h>
+/**   
+ * main - entry point
+ * my hello, printf task
+ * Return: return 1 after program execution
+ */
 
-/**                                                                                                                                     
- *
- *  * main - entry point                                                                                                                   
- *
- *   * my hello, printf task                                                                                                                                     
- *
- *    * Return: return 1 after program execution                                                                                                
- *
- *     */                                                                                                                                     
-
-                                                                                                                                        
-
-int main(void)                                                                                                                          
-
-{                                                                                                                                       
-
-	printf("with proper grammar, but the outcome is a piece of art. \n");                                                                           
-
-	return (0);                                                                                                                             
-
-}                                                                                                                                       
-
+int main(void)                                                                                                                        {
+ printf("with proper grammar, but the outcome is a piece of art. \n");  
+ return (0);
+}
 
 
