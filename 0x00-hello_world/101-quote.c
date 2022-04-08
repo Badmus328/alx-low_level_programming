77da864ece2cbcb96e834da2060631e239008017
Badmus328 <badmusridwa965>
2022-04-08 08:38:21 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * my hello, putchar task
+ * Retur: return 1 after program execution
+ */
+int main(void)
+{
+putchar("and that piece of art is useful"\"  - Dora Korpar'\" , 2015-10-19,\n");
+}
+
