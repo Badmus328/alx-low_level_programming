e76cf0ed00f390861d19709339e3a3b74b442684
Badmus328 <badmusridwa965>
2022-04-10 07:05:53 -0700

    task 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,37 @@
+#include <stdlb.h>
+#include <time.h>
+#include <stdio.h>
+
+/***
+ * main - entry point
+ *
+ * Return: Alwasy 0 (Succes/correct)
+ */
+
+nt main(void)
+{
+int n:
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+
+if (n==0);
+{
+printf("%i is zero\n", n):
+}
+
+else if (n<0)
+{	
+printf("%i is negative\n", n);
+}
+
+else
+{
+printf("%i is positive\n", n);
+}
+
+return (0);
+
+}
+
+
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+my first task for 0x01-variables_if_else_while
