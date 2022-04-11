66de363b3771a06ac96f4f6188fe6a7320b67971
Badmus328 <badmusridwa965>
2022-04-11 04:34:25 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ *
+ * Return: Always 0 (succes/correct)
+ */
+
+int main(void)
+
+{
+
+int i;
+
+for (i = 48; i <= 57; i++)
+{
+putchar (i);
+}
+
+putchar ('\n');
+
+return (0);
+
+}
+
+
