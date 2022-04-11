fb825fbb67775adfb8a10d25c02c9545a5d286ca
Badmus328 <badmusridwa965>
2022-04-11 04:39:34 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main -entry point
+ *
+ * Return: Always 0 (succes/correct)
+ */
+
+int main(void)
+
+{
+
+
+char alphabet;
+
+for (alphabet = 122 ; alphabet >= 97; alphabet--)
+
+{
+putchar(alphabet)
+}
+
+putchar('\n');
+
+return (0);
+
+}
+
+
